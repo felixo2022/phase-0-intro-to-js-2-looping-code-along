2535f2afdb63226e67c03b016cf1e39fdbc7e72f
felixo2022 <walterbarax@gmail.com>
2022-08-25 03:43:02 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards(visitors, gifts) {
+    debugger;
+       let messages=[];
+   
+       for (let i = 0; i < visitors.length; i++) {
+           const message = `Thank you, ${visitors[i]}, for the wonderful ${gifts} gift!`;
+           messages.push(message);
+   
+       }
+       return messages;
+       debugger;
+   }
+   writeCardds(visitors, surprise);
+   writeCards();
+   
+   function countDown(number) {
+       var number= number
+       for (let j = number; j >= 0; j--) {
+           console.log(j);
+         }
+         return number;
+   }
+   countDown(11);
